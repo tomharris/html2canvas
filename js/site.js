bf8b907b1a55fe497deb4addae3ab3912e237a7b
Niklas von Hertzen <niklasvh@gmail.com>
2012-02-26 03:19:27 +0200

    first commit for site

---
+++ js/site.js
@@ -0,0 +1,11 @@
+/* 
+* @author Niklas von Hertzen <niklas at hertzen.com>
+* @created 26.2.2012 
+* @website http://hertzen.com
+ */
+
+
+$(function(){
+    // init prettyprint
+   prettyPrint(); 
+});
